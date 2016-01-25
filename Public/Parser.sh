d124b2b3d25c9f230813809a67fb69ef3fc50513
taochengwei <staugur@saintic.com>
2016-01-25 22:39:40 +0800

    git automation add and push

--- Public/Parser.sh
+++
@@ -1,9 +0,0 @@
-function ParserConf() {
-    if [ $# -ne 3 ];then
-        echo "Usage: ParserConf [config_file] [section] [key]" ; exit 1
-    fi
-    INIFILE=$1; SECTION=$2; KEY=$3
-    value=`awk -F '=' '/['$SECTION']/{a=1}a==1&&$1~/'$KEY'/{print $2;exit}' $INIFILE`
-    echo ${value}
-    return 0
-}
